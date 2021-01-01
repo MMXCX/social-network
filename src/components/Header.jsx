e613339c0c08c8d95c06d97ecd1f93af41bbd087
Mark <g5056866@gmail.com>
2021-01-01 22:58:53 +0300

    Header, Navbar, Profile

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <header className="header">
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/600px-Telegram_logo.svg.png"
+                alt="logo"/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+function Profile() {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"
+                    alt="image"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
