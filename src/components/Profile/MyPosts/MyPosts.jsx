7488cef60adbb33cfd87b0914f895c0d2b86617b
Mark <g5056866@gmail.com>
2021-01-19 07:02:39 +0300

    31 lesson

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,7 @@
 import Post from './Post/Post';
 import classes from './MyPosts.module.css';
 import {Button} from 'antd';
+import React from 'react';
 
 function MyPosts(props) {
 
@@ -8,15 +9,22 @@ function MyPosts(props) {
         el => <Post message={el.message} likesCount={el.likesCount}/>
     );
 
+    let newPostElement = React.createRef();
+
+    function addPost() {
+        let text = newPostElement.current.value;
+        alert(text);
+    }
+
     return (
         <div className={classes.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea name="" id="" cols="30" rows="4"></textarea>
+                    <textarea ref={newPostElement} name="" id="" cols="30" rows="4"></textarea>
                 </div>
                 <div>
-                    <Button type="primary">Add Post</Button>
+                    <Button onClick={addPost} type="primary">Add Post</Button>
                 </div>
             </div>
             <div className={classes.posts}>
