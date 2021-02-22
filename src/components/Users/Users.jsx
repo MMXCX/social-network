1ebba400809c0c7d364ca773924ea0652118140a
Mark <g5056866@gmail.com>
2021-02-22 12:19:42 +0300

    53 lesson. Class Component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,33 +1,37 @@
+import React from 'react';
 import classes from './Users.module.css';
 import UserBlock from './UserBlock/UserBlock.jsx';
 import * as axios from 'axios';
 
-const Users = (props) => {
-    let getUsers = () => {
-        if (props.users.length === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users", {
-                params: {
-                    count: 10,
-                    term: "Mark"
-                }
-            }).then(
-                response => {
-                    console.log(response);
-                    props.setUsers(response.data.items)
-                }
-            );
-        }
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users", {
+            params: {
+                count: 10,
+                term: "Mark"
+            }
+        }).then(
+            response => {
+                console.log(response);
+                this.props.setUsers(response.data.items)
+            }
+        );
     }
 
-    const UsersListEl = props.users.map(
-        (el) => <UserBlock key={el.id} user={el} follow={props.follow} unFollow={props.unFollow}/>
-    );
-    return (
-        <div>
-            <button onClick={getUsers}>Get Users</button>
-            {UsersListEl}
-        </div>
-    );
+    render() {
+        let UsersListEl = this.props.users.map(
+            (el) => <UserBlock key={el.id} user={el} follow={this.props.follow} unFollow={this.props.unFollow}/>
+        );
+
+        return (
+            <div>
+                {UsersListEl}
+            </div>
+        );
+    }
+
 }
 
 export default Users;
